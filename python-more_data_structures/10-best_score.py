e66adc0a7113f226883c14624c15129d288c7319
alejandrocolombani <alejandrojcolombani@gmail.com>
2026-05-15 16:01:56 -0400

    added task 10

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    best_score = 0
+    best_key = ''
+    for key in a_dictionary:
+        if a_dictionary[key] > best_score:
+            best_score = a_dictionary[key]
+            best_key = key
+    return best_key
