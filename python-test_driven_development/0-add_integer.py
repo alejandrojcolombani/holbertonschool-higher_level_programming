54616c6f5180e884500e120717c90c6093185045
alejandrocolombani <alejandrojcolombani@gmail.com>
2026-05-17 22:47:48 -0400

    added task 0

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module contains a function that adds two integers."""
+
+
+def add_integer(a, b=98):
+    """Returns the addition of two integers."""
+
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
