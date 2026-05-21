20cc3061a082e05b9eec2d82eeed81c5a90518f3
alejandrocolombani <alejandrojcolombani@gmail.com>
2026-05-21 16:09:31 -0400

    added task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module defines a Square class."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size):
+        """Initializes the square with a size."""
+        self.__size = size
