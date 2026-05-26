1d66c55043fc3eeb5b07595870d1709fab074472
alejandrocolombani <alejandrojcolombani@gmail.com>
2026-05-26 18:47:28 -0400

    added task 4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
+"""This module contains a function that checks inherited instances."""
+
+
 def inherits_from(obj, a_class):
+    """Returns True if obj inherits from a_class, otherwise False."""
+
     return isinstance(obj, a_class) and type(obj) is not a_class
