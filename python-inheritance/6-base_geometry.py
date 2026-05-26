db3ce17e08d80533490dee4ae33a611bf7bde88c
alejandrocolombani <alejandrojcolombani@gmail.com>
2026-05-26 18:54:48 -0400

    added task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module defines a BaseGeometry class."""
+
+
+class BaseGeometry:
+    """Represents a base geometry class."""
+
+    def area(self):
+        """Raises an exception because area is not implemented."""
+        raise Exception("area() is not implemented")
